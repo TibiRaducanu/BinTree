436aaa691c15389badcda13daa91bf2b04678cc0
Ana <ana.maria.adina98@gmail.com>
2019-04-21 14:20:31 +0300

    GirlPlayerMovement
    Added animations for every move of the player and a script for controlling the moves.

---
+++ BinTree/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Animator playerAnimator;
+    private bool isMoving;
+    private Vector2 lastMove;
+
+    private Rigidbody2D playerRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        lastMove = new Vector2();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isMoving = false;
+
+        if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+
+            playerRigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, playerRigidBody.velocity.y);
+            isMoving = true;
+            lastMove.x = Input.GetAxisRaw("Horizontal");
+            lastMove.y = 0f;
+        }
+
+        if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+
+            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+            isMoving = true;
+            lastMove.y = Input.GetAxisRaw("Vertical");
+            lastMove.x = 0f;
+        }
+
+        if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+        {
+            playerRigidBody.velocity = new Vector2(0f, playerRigidBody.velocity.y);
+        }
+
+        if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+        {
+            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, 0f);
+        }
+
+        playerAnimator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        playerAnimator.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        playerAnimator.SetBool("IsMoving", isMoving);
+        playerAnimator.SetFloat("LastMoveX", lastMove.x);
+        playerAnimator.SetFloat("LastMoveY", lastMove.y);
+    }
+}
