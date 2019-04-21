16025d2d64ce566ae2a7d6d7a13c707e1d7c4ba9
TibiRaducanu <tibi.raducanu@gmail.com>
2019-04-21 16:21:31 +0300

    Added Boss Fight Level1
    Added Boss Fight Level1 & /uploaded Art File

---
+++ BinTree/Assets/Scripts/SpawnAnnoyingEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAnnoyingEnemy : MonoBehaviour
+{
+    public float spawnInterval;
+    private float spawnIntervalCount;
+    public Object objectToSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+        spawnIntervalCount = spawnInterval;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnIntervalCount -= Time.deltaTime;
+        if (spawnIntervalCount < 0f)
+        {
+            Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+            spawnIntervalCount = spawnInterval;
+        }
+    }
+}
