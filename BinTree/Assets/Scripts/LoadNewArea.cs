ed4eda0935d480e04391146277e833c4410a3205
TibiRaducanu <tibi.raducanu@gmail.com>
2019-04-22 11:18:06 +0300

    Created demo room.
    Added 3 doors to the demo room.

--- BinTree/Assets/Scripts/LoadNewArea.cs
+++ BinTree/Assets/Scripts/LoadNewArea.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class LoadNewArea : MonoBehaviour
 {
     public string levelToLoad;
+    private bool readyToLoad;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        readyToLoad = true;
     }
 
     // Update is called once per frame
@@ -18,9 +19,14 @@ public class LoadNewArea : MonoBehaviour
 
     }
 
+    void ReadyToLoad()
+    {
+        readyToLoad = true;
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player" && readyToLoad)
         {
             Application.LoadLevel(levelToLoad);
         }
